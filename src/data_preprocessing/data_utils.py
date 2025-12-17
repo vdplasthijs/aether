044fcb9e9cef12213b2f9dd8ff872deb0f7f8c95
GabrieleTi <gabriele.tijunaityte@gmail.com>
2025-12-17 11:02:04 +0100

    Corine and bioclim column explanations processing once and saving

--- src/data_preprocessing/data_utils.py
+++ src/data_preprocessing/data_utils.py
@@ -1,13 +1,16 @@
 import os, datetime, ast
-import numpy as np 
-import pandas as pd 
+import json
+import numpy as np
+import pandas as pd
 import geopandas as gpd
 import rasterio
 import xarray as xr
 import rioxarray as rxr
 from hydra import initialize, compose
-from omegaconf import DictConfig, OmegaConf
+from omegaconf import OmegaConf
 
+
+# TODO: is this still used? works?
 def get_hydra_paths():
     assert False, 'Deprecated. Use hydra config or environment variables instead.'
     cwd = os.getcwd() + '/..'
@@ -21,104 +24,100 @@ def get_hydra_paths():
                 f"hydra.job.num=0",  # required to resolve hydra.job.* interpolations
                 f"output_dir={output_dir}",
                 f"work_dir={cwd}",
-            ]
+            ],
         )
 
     resolved_cfg = OmegaConf.to_container(cfg, resolve=True)
-    path_dict = resolved_cfg#['paths']
+    path_dict = resolved_cfg  # ['paths']
     path_dict['repo'] = path_dict['root_dir']
     return path_dict
 
-def corine_lc_schema():
-    '''From https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_CORINE_V20_100m#bands'''
-    corine_classes = [
-        {"code": 111, "color": "#e6004d", "category": "Artificial surfaces > Urban fabric > Continuous urban fabric"},
-        {"code": 112, "color": "#ff0000", "category": "Artificial surfaces > Urban fabric > Discontinuous urban fabric"},
-        {"code": 121, "color": "#cc4df2", "category": "Artificial surfaces > Industrial, commercial, and transport units > Industrial or commercial units"},
-        {"code": 122, "color": "#cc0000", "category": "Artificial surfaces > Industrial, commercial, and transport units > Road and rail networks and associated land"},
-        {"code": 123, "color": "#e6cccc", "category": "Artificial surfaces > Industrial, commercial, and transport units > Port areas"},
-        {"code": 124, "color": "#e6cce6", "category": "Artificial surfaces > Industrial, commercial, and transport units > Airports"},
-        {"code": 131, "color": "#a600cc", "category": "Artificial surfaces > Mine, dump, and construction sites > Mineral extraction sites"},
-        {"code": 132, "color": "#a64dcc", "category": "Artificial surfaces > Mine, dump, and construction sites > Dump sites"},
-        {"code": 133, "color": "#ff4dff", "category": "Artificial surfaces > Mine, dump, and construction sites > Construction sites"},
-        {"code": 141, "color": "#ffa6ff", "category": "Artificial surfaces > Artificial, non-agricultural vegetated areas > Green urban areas"},
-        {"code": 142, "color": "#ffe6ff", "category": "Artificial surfaces > Artificial, non-agricultural vegetated areas > Sport and leisure facilities"},
-        {"code": 211, "color": "#ffffa8", "category": "Agricultural areas > Arable land > Non-irrigated arable land"},
-        {"code": 212, "color": "#ffff00", "category": "Agricultural areas > Arable land > Permanently irrigated land"},
-        {"code": 213, "color": "#e6e600", "category": "Agricultural areas > Arable land > Rice fields"},
-        {"code": 221, "color": "#e68000", "category": "Agricultural areas > Permanent crops > Vineyards"},
-        {"code": 222, "color": "#f2a64d", "category": "Agricultural areas > Permanent crops > Fruit trees and berry plantations"},
-        {"code": 223, "color": "#e6a600", "category": "Agricultural areas > Permanent crops > Olive groves"},
-        {"code": 231, "color": "#e6e64d", "category": "Agricultural areas > Pastures > Pastures"},
-        {"code": 241, "color": "#ffe6a6", "category": "Agricultural areas > Heterogeneous agricultural areas > Annual crops associated with permanent crops"},
-        {"code": 242, "color": "#ffe64d", "category": "Agricultural areas > Heterogeneous agricultural areas > Complex cultivation patterns"},
-        {"code": 243, "color": "#e6cc4d", "category": "Agricultural areas > Heterogeneous agricultural areas > Land principally occupied by agriculture, with significant areas of natural vegetation"},
-        {"code": 244, "color": "#f2cca6", "category": "Agricultural areas > Heterogeneous agricultural areas > Agro-forestry areas"},
-        {"code": 311, "color": "#80ff00", "category": "Forest and semi natural areas > Forests > Broad-leaved forest"},
-        {"code": 312, "color": "#00a600", "category": "Forest and semi natural areas > Forests > Coniferous forest"},
-        {"code": 313, "color": "#4dff00", "category": "Forest and semi natural areas > Forests > Mixed forest"},
-        {"code": 321, "color": "#ccf24d", "category": "Forest and semi natural areas > Scrub and/or herbaceous vegetation associations > Natural grasslands"},
-        {"code": 322, "color": "#a6ff80", "category": "Forest and semi natural areas > Scrub and/or herbaceous vegetation associations > Moors and heathland"},
-        {"code": 323, "color": "#a6e64d", "category": "Forest and semi natural areas > Scrub and/or herbaceous vegetation associations > Sclerophyllous vegetation"},
-        {"code": 324, "color": "#a6f200", "category": "Forest and semi natural areas > Scrub and/or herbaceous vegetation associations > Transitional woodland-shrub"},
-        {"code": 331, "color": "#e6e6e6", "category": "Forest and semi natural areas > Open spaces with little or no vegetation > Beaches, dunes, sands"},
-        {"code": 332, "color": "#cccccc", "category": "Forest and semi natural areas > Open spaces with little or no vegetation > Bare rocks"},
-        {"code": 333, "color": "#ccffcc", "category": "Forest and semi natural areas > Open spaces with little or no vegetation > Sparsely vegetated areas"},
-        {"code": 334, "color": "#000000", "category": "Forest and semi natural areas > Open spaces with little or no vegetation > Burnt areas"},
-        {"code": 335, "color": "#a6e6cc", "category": "Forest and semi natural areas > Open spaces with little or no vegetation > Glaciers and perpetual snow"},
-        {"code": 411, "color": "#a6a6ff", "category": "Wetlands > Inland wetlands > Inland marshes"},
-        {"code": 412, "color": "#4d4dff", "category": "Wetlands > Inland wetlands > Peat bogs"},
-        {"code": 421, "color": "#ccccff", "category": "Wetlands > Maritime wetlands > Salt marshes"},
-        {"code": 422, "color": "#e6e6ff", "category": "Wetlands > Maritime wetlands > Salines"},
-        {"code": 423, "color": "#a6a6e6", "category": "Wetlands > Maritime wetlands > Intertidal flats"},
-        {"code": 511, "color": "#00ccf2", "category": "Water bodies > Inland waters > Water courses"},
-        {"code": 512, "color": "#80f2e6", "category": "Water bodies > Inland waters > Water bodies"},
-        {"code": 521, "color": "#00ffa6", "category": "Water bodies > Marine waters > Coastal lagoons"},
-        {"code": 522, "color": "#a6ffe6", "category": "Water bodies > Marine waters > Estuaries"},
-        {"code": 523, "color": "#e6f2ff", "category": "Water bodies > Marine waters > Sea and ocean"},
-    ]
 
-    dict_all = {x: [] for x in ['code', 'color', 'category', 'category_level_1', 'category_level_2', 'category_level_3']}
+def process_corine_classes(input_path, output_path):
+    '''Creates processed csv file with all corine classes'''
+
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f'File {input_path} for Corine classes does not exist')
+
+    # read-in org file
+    with open(input_path, 'r') as f:
+        corine_classes = json.load(f)
+
+    # format it into pandas
+    keys = list(corine_classes[0].keys())
+    keys.extend(['category_level_1', 'category_level_2', 'category_level_3'])
+
+    # collect all keys and vals, split descriptions based on categories
+    dict_all = {x: [] for x in keys}
     for item in corine_classes:
-        for key, val in item.items():   
+        for key, val in item.items():
+            if key == 'code':
+                val = f'aux_corine_frac_{val}'
             dict_all[key].append(val)
             if key == 'category':
                 levels = val.split(' > ')
                 dict_all['category_level_1'].append(levels[0])
                 dict_all['category_level_2'].append(levels[1])
                 dict_all['category_level_3'].append(levels[2])
-    df_all = pd.DataFrame(dict_all)
-    return corine_classes, df_all
+    df = pd.DataFrame(dict_all)
 
-def bioclim_schema():
+    # save csv
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+    print(f'Processed Corine classes were saved to {output_path}')
+
+
+def process_bioclim_classes(input_path, output_path):
+    '''Creates processed csv file with all bioclim classes'''
+
+    if not os.path.exists(input_path):
+        raise FileNotFoundError(f'File {input_path} for bioclimatic scheme does not exist')
+
+    with open(input_path, 'r') as f:
+        bioclim_classes = json.load(f)
+
+    # Create and save pandas df
+    df = pd.DataFrame(bioclim_classes)
+    df['name'] = df['name'].apply(lambda x: x.replace('bio', 'aux_bioclim_'))
+
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    df.to_csv(output_path, index=False)
+    print(f'Processed bioclimatic classes were saved to {output_path}')
+
+
+def corine_lc_schema(data_dir='data/'):
+    '''From https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_CORINE_V20_100m#bands'''
+    if not os.path.isfile(os.path.join(data_dir, "caption_templates/corine_classes.csv")):
+        process_corine_classes(
+            os.path.join(data_dir, "source/corine_classes.json"),
+            os.path.join(data_dir, "caption_templates/corine_classes.csv"),
+        )
+    df = pd.read_csv(os.path.join(data_dir, ("caption_templates/corine_classes.csv")))
+
+    corine_classes = df.to_dict('records')
+    for c in corine_classes:
+        c['code'] = int(c['code'].replace('aux_corine_frac_', ''))
+
+    return corine_classes, df
+
+
+def bioclim_schema(data_dir='data/'):
     '''From https://developers.google.com/earth-engine/datasets/catalog/WORLDCLIM_V1_BIO'''
-    bioclim_variables = [
-        {"name": "bio01", "units": "°C", "min": -29, "max": 32, "scale": 0.1, "pixel_size": "meters", "description": "Annual mean temperature"},
-        {"name": "bio02", "units": "°C", "min": 0.9, "max": 21.4, "scale": 0.1, "pixel_size": "meters", "description": "Mean diurnal range (mean of monthly (max temp - min temp))"},
-        {"name": "bio03", "units": "%", "min": 7, "max": 96, "scale": 1, "pixel_size": "meters", "description": "Isothermality (bio02/bio07 * 100)"},
-        {"name": "bio04", "units": "°C", "min": 0.62, "max": 227.21, "scale": 0.01, "pixel_size": "meters", "description": "Temperature seasonality (Standard deviation * 100)"},
-        {"name": "bio05", "units": "°C", "min": -9.6, "max": 49, "scale": 0.1, "pixel_size": "meters", "description": "Max temperature of warmest month"},
-        {"name": "bio06", "units": "°C", "min": -57.3, "max": 25.8, "scale": 0.1, "pixel_size": "meters", "description": "Min temperature of coldest month"},
-        {"name": "bio07", "units": "°C", "min": 5.3, "max": 72.5, "scale": 0.1, "pixel_size": "meters", "description": "Temperature annual range (bio05 - bio06)"},
-        {"name": "bio08", "units": "°C", "min": -28.5, "max": 37.8, "scale": 0.1, "pixel_size": "meters", "description": "Mean temperature of wettest quarter"},
-        {"name": "bio09", "units": "°C", "min": -52.1, "max": 36.6, "scale": 0.1, "pixel_size": "meters", "description": "Mean temperature of driest quarter"},
-        {"name": "bio10", "units": "°C", "min": -14.3, "max": 38.3, "scale": 0.1, "pixel_size": "meters", "description": "Mean temperature of warmest quarter"},
-        {"name": "bio11", "units": "°C", "min": -52.1, "max": 28.9, "scale": 0.1, "pixel_size": "meters", "description": "Mean temperature of coldest quarter"},
-        {"name": "bio12", "units": "mm", "min": 0, "max": 11401, "scale": 1, "pixel_size": "meters", "description": "Annual precipitation"},
-        {"name": "bio13", "units": "mm", "min": 0, "max": 2949, "scale": 1, "pixel_size": "meters", "description": "Precipitation of wettest month"},
-        {"name": "bio14", "units": "mm", "min": 0, "max": 752, "scale": 1, "pixel_size": "meters", "description": "Precipitation of driest month"},
-        {"name": "bio15", "units": "Coefficient of Variation", "min": 0, "max": 265, "scale": 1, "pixel_size": "meters", "description": "Precipitation seasonality"},
-        {"name": "bio16", "units": "mm", "min": 0, "max": 8019, "scale": 1, "pixel_size": "meters", "description": "Precipitation of wettest quarter"},
-        {"name": "bio17", "units": "mm", "min": 0, "max": 2495, "scale": 1, "pixel_size": "meters", "description": "Precipitation of driest quarter"},
-        {"name": "bio18", "units": "mm", "min": 0, "max": 6090, "scale": 1, "pixel_size": "meters", "description": "Precipitation of warmest quarter"},
-        {"name": "bio19", "units": "mm", "min": 0, "max": 5162, "scale": 1, "pixel_size": "meters", "description": "Precipitation of coldest quarter"},
-    ]
-    dict_all = {x: [] for x in ['name', 'units', 'min', 'max', 'scale', 'pixel_size', 'description']}
-    for item in bioclim_variables:
-        for key, val in item.items():   
-            dict_all[key].append(val)
-    df_all = pd.DataFrame(dict_all)
-    return bioclim_variables, df_all
+
+    if not os.path.isfile(os.path.join(data_dir, "caption_templates/bioclim_classes.csv")):
+        process_bioclim_classes(
+            os.path.join(data_dir, "source/bioclim_classes.json"),
+            os.path.join(data_dir, "caption_templates/bioclim_classes.csv"),
+        )
+
+    df = pd.read_csv(os.path.join(data_dir, "caption_templates/bioclim_classes.csv"))
+    df.sort_values(by=["name"], inplace=True)
+    bioclim_variables = df.to_dict('records')
+    for v in bioclim_variables:
+        v['name'] = v['name'].replace('aux_bioclim_', 'bio')
+
+    return bioclim_variables, df
+
 
 def get_path_s2bms():
     """Get the path to the Sentinel-2 BMS data directory."""
@@ -130,9 +129,13 @@ def get_path_s2bms():
     if 'S2BMS_PRESENCE' in os.environ:
         presence_path = os.environ.get('S2BMS_PRESENCE')
     else:
-        presence_path = os.path.join(os.environ.get('PROJECT_ROOT', '.'), 'data/source/butterflies/S2BMS/ukbms_species-presence/bms_presence_y-2018-2019_th-200.shp')
+        presence_path = os.path.join(
+            os.environ.get('PROJECT_ROOT', '.'),
+            'data/source/butterflies/S2BMS/ukbms_species-presence/bms_presence_y-2018-2019_th-200.shp',
+        )
     assert os.path.exists(presence_path), f"Sentinel-2 BMS presence path does not exist: {presence_path}"
     return im_path, presence_path
+
 
 def load_s2bms_presence():
     """Load the Sentinel-2 BMS species presence GeoDataFrame."""
@@ -142,16 +145,20 @@ def load_s2bms_presence():
     df_s2bms_presence = df_s2bms_presence.to_crs(epsg=4326)
     df_s2bms_presence['lat'] = df_s2bms_presence.geometry.y
     df_s2bms_presence['lon'] = df_s2bms_presence.geometry.x
-    df_s2bms_presence['tuple_coords'] = [tuple([ast.literal_eval(x) for x in df_s2bms_presence.tuple_coor[ii].lstrip('(').rstrip(')').split(', ')]) for ii in range(len(df_s2bms_presence))]
+    df_s2bms_presence['tuple_coords'] = [
+        tuple([ast.literal_eval(x) for x in df_s2bms_presence.tuple_coor[ii].lstrip('(').rstrip(')').split(', ')])
+        for ii in range(len(df_s2bms_presence))
+    ]
     df_s2bms_presence.drop(columns=['row_id', 'tuple_coor'], inplace=True)
     return df_s2bms_presence
+
 
 def load_tiff(tiff_file_path, datatype='np', verbose=0):
     '''Load tiff file as np or da'''
     with rasterio.open(tiff_file_path) as f:
         if verbose > 0:
             print(f.profile)
-        if datatype == 'np':  # handle different file types 
+        if datatype == 'np':  # handle different file types
             im = f.read()
             assert type(im) == np.ndarray
         elif datatype == 'da':
@@ -159,7 +166,8 @@ def load_tiff(tiff_file_path, datatype='np', verbose=0):
             assert type(im) == xr.DataArray
         else:
             assert False, 'datatype should be np or da'
-    return im 
+    return im
+
 
 def create_timestamp(include_seconds=False):
     dt = datetime.datetime.now()
@@ -168,14 +176,15 @@ def create_timestamp(include_seconds=False):
         timestamp += ':' + str(dt.second).zfill(2)
     return timestamp
 
-def load_aux_data(filepath='../data/model_ready/s2bms_bioclim_lc_data.csv',
-                  col_identifier='name'):
+
+def load_aux_data(filepath='../data/model_ready/s2bms_bioclim_lc_data.csv', col_identifier='name'):
     """Load auxiliary bioclimatic and land cover data."""
     assert os.path.exists(filepath), f"Auxiliary data file does not exist: {filepath}"
     df_aux = pd.read_csv(filepath)
     if col_identifier is not None:
         assert col_identifier in df_aux.columns, f"Column identifier '{col_identifier}' not found in auxiliary data."
     return df_aux
+
 
 def get_article(str_follow):
     assert type(str_follow) == str, f'str_follow must be a string, but got {type(str_follow)}'
@@ -184,6 +193,7 @@ def get_article(str_follow):
         return 'an ' + str_follow
     else:
         return 'a ' + str_follow
+
 
 if __name__ == "__main__":
     print('This is a utility script for creating and processing the dataset.')
