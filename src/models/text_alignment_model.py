cb95fee313b2d8a64d321996ba73cd8124692bfe
GabrieleTi <gabriele.tijunaityte@gmail.com>
2026-01-07 11:26:47 +0100

    Add similarity metrix and fix clip loss

--- src/models/text_alignment_model.py
+++ src/models/text_alignment_model.py
@@ -1,6 +1,7 @@
 from typing import override, Dict, Tuple
 
 import torch
+import torch.nn.functional as F
 
 from src.models.base_model import BaseModel
 from src.models.components.eo_encoders.base_eo_encoder import BaseEOEncoder
@@ -48,6 +49,8 @@ class TextAlignmentModel(BaseModel):
             batch: Dict[str, torch.Tensor],
             mode: str = 'train',
     ) -> Tuple[torch.Tensor, torch.Tensor]:
+
+        # EMbed modalities
         eo_feats = self.eo_encoder(batch)
         text_feats = self.text_encoder(batch, mode)
         return eo_feats, text_feats
@@ -58,11 +61,44 @@ class TextAlignmentModel(BaseModel):
             batch: Dict[str, torch.Tensor],
             mode: str='train'
     ) -> torch.Tensor:
+        # Embed
         eo_feats, text_feats = self.forward(batch, mode)
-        loss = self.loss_fn(eo_feats, text_feats)
 
+        # Get similarities
+        _ = self._cos_sim_calc(eo_feats, text_feats, mode)
+
+        # Get loss
+        loss = self.loss_fn(eo_feats, text_feats)
         self.log(f"{mode}_loss", loss, on_step=False, on_epoch=True, prog_bar=True)
+        if self.loss_fn.__getattr__('log_temp'):
+            self.log(f'{mode}_loss_temp', self.loss_fn.__getattr__('log_temp').exp(), on_step=False, on_epoch=True, prog_bar=True)
+
         return loss
+
+    def _cos_sim_calc(self, eo_embeds, text_embeds, mode, log=True):
+
+        # Similarity matrx
+        cos_sim_matrix = F.cosine_similarity(eo_embeds[:, None, :], text_embeds[None, :, :], dim=-1)
+
+        # Average for positive and negative pairs
+        # TODO change label option if we change what gets treated to be pos/neg
+        id_matrix = torch.eye(cos_sim_matrix.shape[0], dtype=torch.bool)
+        pos_sim = cos_sim_matrix[id_matrix]
+        neg_sim = cos_sim_matrix[~id_matrix]
+
+        # Average
+        avr_sim = torch.mean(cos_sim_matrix)
+        sub_neg_sim = neg_sim[torch.randperm(len(neg_sim))[:len(pos_sim)]] # pick same amount of negatives as positives
+        balanced_sim = torch.cat([pos_sim, sub_neg_sim], dim=0)
+        balanced_avr_sim = torch.mean(balanced_sim)
+
+        if log:
+            self.log(f'{mode}_avr_sim', avr_sim, on_step=False, on_epoch=True, prog_bar=True)
+            self.log(f'{mode}_avr_sim_balanced', balanced_avr_sim, on_step=False, on_epoch=True, prog_bar=True)
+            self.log(f'{mode}_pos_sim', torch.mean(pos_sim), on_step=False, on_epoch=True, prog_bar=True)
+            self.log(f'{mode}_neg_sim', torch.mean(neg_sim), on_step=False, on_epoch=True, prog_bar=True)
+        return avr_sim, pos_sim, neg_sim
+
 
 if __name__ == "__main__":
     _ = TextAlignmentModel(None, None, None, None, None, None, None)
